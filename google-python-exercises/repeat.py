8b5121c56ceb36d1bb6009ece9c22739e613cabc
estherdomfeh213 <domfehesther2020@gmail.com>
2025-10-17 08:02:02 +0000

    intro to user defined functions

---
+++ google-python-exercises/repeat.py
@@ -0,0 +1,16 @@
+# Defines a 'repeat' function that takes 2 arguments. 
+def repeat(s, exclaim):
+    """
+    Returns the string 's' repeated 3 times.
+    If exclaim is true, add exclamation marks. 
+    """
+
+    result = s * 3 
+    if exclaim: 
+        result = result + '!!!'
+    return result
+
+def main():
+    print(repeat('Yay', False))
+    print(repeat('Woo Hoo', True))
+
