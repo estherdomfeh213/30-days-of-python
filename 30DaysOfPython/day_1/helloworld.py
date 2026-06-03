6a0c256ee2ba10d5436fc1d755e4b32212ee15fc
estherdomfeh213 <domfehesther2020@gmail.com>
2026-06-03 17:56:59 +0000

    add: day_1 challenge done

---
+++ 30DaysOfPython/day_1/helloworld.py
@@ -0,0 +1,18 @@
+# Day 1 - 30DaysOfPython Challenge
+
+print(2 + 3)  # addition(+)
+print(3 - 1) # subtraction(-)
+print(2 * 3) # multiplication(*)
+print(3 / 2) # division(/)
+print(3 % 2) # modulus(%)
+print(3 // 2) # Floor division(//)
+
+# Checking data types
+print(type(10))  # int
+print(type(3.14))  # float
+print(type(1 + 3j))  # complex number
+print(type('Esther'))  # string
+print(type([1,2,3]))  # list
+print(type({'name' : 'Esther'}))  # dictionary
+print(type({9.8, 3.14, 2.7}))  # set
+print(type((9.8, 3.14, 2.7)) ) # tuple
