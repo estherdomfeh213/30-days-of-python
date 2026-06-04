e90a03c1dbe98c12526a814ee03f550dfdf87047
estherdomfeh213 <domfehesther2020@gmail.com>
2026-06-04 21:06:03 +0000

    add: day_2 level2 excercise_1 data types

---
+++ 30DaysOfPython/day_2/variables.py
@@ -0,0 +1,30 @@
+# Day 2: 30 Days of Python programming
+
+first_name = 'Miley'
+last_name = 'Chris'
+full_name = 'Miley Chris'
+country = 'Belgium'
+city = 'Brussels'
+age = 30
+year = 2024
+is_married = False
+is_true = True
+is_light_on = True
+fav_food, music, color, vacation = 'pasta', 'pop', 'pink', 'singapore'
+
+# Exercises: Level 2
+# 1: Check the data type of all your variables using type() built-in function.
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(fav_food))
+print(type(music))
+print(type(color))
+print(type(vacation))   
